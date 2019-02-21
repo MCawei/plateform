62a2d57c2aef0919c435b8dea17e79b6ec096201
Willsen <cwillsen129@163.com>
2019-02-21 11:57:41 +0800

    api

--- src/api/http.js
+++ src/api/http.js
@@ -1,5 +1,6 @@
 'use strict'
 import Axios from 'axios';
+import QS from 'qs';
 import { Message } from 'element-ui'
 
 // Axios.defaults.withCredentials = true
@@ -12,8 +13,8 @@ const axios = Axios.create({
 });
 
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-// axios.defaults.timeout =  6000;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.timeout =  10000;
 
 export const apiGet = (url, params) => {
   return new Promise((resolve, reject) => {
@@ -56,25 +57,75 @@ export const apiDelete = (url, params) => {
   })
 };
 
-// http请求拦截器 请求之前的一些操作
-Axios.interceptors.request.use(config => {
-  if(config.method=='post'){
-    config.data=QS.stringify(config.data); //防止post请求参数无法传到后台
-  }
-  return config
-}, error => {
-  Message.error({
-    message: '加载超时'
+// http请求拦截器 请求之前的操作
+Axios.interceptors.request.use(
+  config => {
+    // const token = store.state.token;
+    // token && (config.headers.Authorization = token);
+    return config;
+  }, error => {
+    Message.error({
+      message: '响应超时'
+    });
+    return Promise.reject(error)
   });
-  return Promise.reject(error)
-});
-// http响应拦截器  请求之后的操作
-Axios.interceptors.response.use(data => {
-  return data
-}, error => {
-  Message.error({
-    message: '加载失败'
-  });
-  return Promise.reject(error)
-});
 
+// http响应拦截器  请求响应之后的操作
+axios.interceptors.response.use(
+  response => {
+    if (response.status === 200) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(response);
+    }
+  },
+  // 服务器状态码不是200的情况
+  error => {
+    if (error.response.status) {
+      switch (error.response.status) {
+        // 401: 未登录
+        // 未登录则跳转登录页面，并携带当前页面的路径
+        // 在登录成功后返回当前页面，这一步需要在登录页操作。
+        case 401:
+          router.replace({
+            path: '/login',
+            query: { redirect: router.currentRoute.fullPath }
+          });
+          break;
+        // 403 token过期
+        // 登录过期对用户进行提示
+        // 清除本地token和清空vuex中token对象
+        // 跳转登录页面
+        case 403:
+          Message.error({
+            message: '登录过期，请重新登录'
+          });
+          // 清除token
+          localStorage.removeItem('token');
+          // store.commit('loginSuccess', null);
+          // 跳转登录页面，并将要浏览的页面fullPath传过去，登录成功后跳转需要访问的页面
+          setTimeout(() => {
+            router.replace({
+              path: '/login',
+              query: {
+                redirect: router.currentRoute.fullPath
+              }
+            });
+          }, 1000);
+          break;
+        // 404请求不存在
+        case 404:
+          Message.error({
+            message: '网络请求不存在'
+          });
+          break;
+        // 其他错误，直接抛出错误提示
+        default:
+          Message.error({
+            message: error.response.data.message
+          });
+      }
+      return Promise.reject(error.response);
+    }
+  }
+);
